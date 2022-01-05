e4e88cbefdf5a71fab79f1d8dd7cdf3f29993eec
rylee816 <65700028+rylee816@users.noreply.github.com>
2022-01-05 17:10:37 -0500

    replace all Home.jsx images with proper path

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -10,13 +10,13 @@ function Cards() {
             <div className="cards__wrapper">
                 <ul className="cards__items">
                 <CardItem
-              src='images/img-9.jpg'
+              src='React-Travel-Demo-Website/images/img-9.jpg'
               text='Explore the hidden waterfall deep inside the Amazon Jungle'
               label='Adventure'
               path="https://www.tripadvisor.com/Attractions-g793795-Activities-c57-t95-Amazon_River_State_of_Amazonas.html"
             />
             <CardItem
-              src='images/img-2.jpg'
+              src='React-Travel-Demo-Website/images/img-2.jpg'
               text='Travel through the Islands of Bali in a Private Cruise'
               label='Luxury'
               path="https://www.tripadvisor.com/Attractions-g294226-Activities-c57-t20-Bali.html"
@@ -24,19 +24,19 @@ function Cards() {
                 </ul>
                 <ul className='cards__items'>
                 <CardItem
-              src='images/img-3.jpg'
+              src='React-Travel-Demo-Website/images/img-3.jpg'
               text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
               label='Mystery'
               path="https://vacationidea.com/florida/best-things-to-do-in-hollywood-florida.html"
             />
             <CardItem
-              src='images/img-4.jpg'
+              src='React-Travel-Demo-Website/images/img-4.jpg'
               text='Experience Football on Top of the Himilayan Mountains'
               label='Adventure'
               path="https://www.tripadvisor.com/ShowUserReviews-g315763-d12425860-r552854823-Hotel_Mystic_Mountain-Nagarkot_Bagmati_Zone_Central_Region.html"
             />
             <CardItem
-              src='images/img-8.jpg'
+              src='React-Travel-Demo-Website/images/img-8.jpg'
               text='Ride through the Sahara Desert on a guided camel tour'
               label='Adrenaline'
               path="https://www.tripadvisor.com/AttractionProductReview-g304017-d19010203-Camel_Ride_And_Overnight_in_The_Sahara_Desert-Merzouga_Draa_Tafilalet.html"
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ function Hero() {
 
     return (
         <div className='hero-container'>
-            <video src="https://rylee816.github.io/React-Travel-Demo-Website/images/video-2.mp4" autoPlay loop muted></video>
+            <video src="React-Travel-Demo-Website/images/video-2.mp4" autoPlay loop muted></video>
             {/* <video src="https://player.vimeo.com/external/387675206.sd.mp4?s=59fb02dd43b656dd886ec540ff23a0aa20685922&profile_id=164" autoPlay loop muted></video> */}
             <h1>ADVENTURE CALLS</h1>
             <p>What are you waiting for? <span className='xplor'>XPLOR.</span></p>

8a9ae2537930a8e1970bef4c17754e781e0d6e40
rylee816 <65700028+rylee816@users.noreply.github.com>
2022-01-05 12:15:59 -0500

    package.json experiment with App.js changed to 'home'

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/products" element={<Products />} />
           <Route path="/services" element={<Services />} />
           <Route path="/sign-up" element={<SignUp />} />
